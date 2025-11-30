04de2e56f745d1affaafd696a60889eade733183
Shivani <shivani18905@gmail.com>
2025-11-30 12:56:14 +0530

    added 2 files

--- src/main/java/in/kmit/MyJavaProject/App.java
+++ src/main/java/in/kmit/MyJavaProject/App.java
@@ -6,6 +6,6 @@ package in.kmit.MyJavaProject;
 public class App {
     public static void main(String[] args) {
         System.out.println(" \nHello Welcome to the World of Mathematics!\n");
-        System.out.println(" \n\n Let us play with Numbers \n");
+        System.out.println(" \n\n Let us play with Numbers \n\n");
     }
 }
